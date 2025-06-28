de33229a345f490802e5307fbc5f6b23ac0a7e44
sharina-ya <uliasarina7@gmail.com>
2025-06-28 19:46:27 +1000

    Add files via upload

--- app/src/main/java/co/feip/fefu2025/presentation/animeDetails/AnimeDetailsViewModel.kt
+++ app/src/main/java/co/feip/fefu2025/presentation/animeDetails/AnimeDetailsViewModel.kt
@@ -1,32 +1,16 @@
 package co.feip.fefu2025.presentation.animeDetails
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import co.feip.fefu2025.data.repository.MockAnimeRepository
 import co.feip.fefu2025.domain.model.Anime
-import co.feip.fefu2025.domain.repository.AnimeRepository
-import co.feip.fefu2025.domain.usecase.GetAnimeDetailsUseCase
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 
-class AnimeDetailsViewModel(
-    private val getAnimeDetailsUseCase: GetAnimeDetailsUseCase = GetAnimeDetailsUseCase()
-) : ViewModel() {
+class AnimeDetailsViewModel : ViewModel() {
+    private val _anime = MutableStateFlow<Anime?>(null)
+    val anime: StateFlow<Anime?> = _anime
 
-    private val _anime = MutableLiveData<Anime>()
-    val anime: LiveData<Anime> = _anime
-
-    // Загрузка деталей по объекту Anime
-    fun loadAnimeDetails(anime: Anime) {
-        val details = getAnimeDetailsUseCase.execute(anime)
-        _anime.value = details
-    }
-
-    // Загрузка деталей по id (использует репозиторий аниме)
-    fun loadAnimeDetailsById(id: Int) {
-        val anime = AnimeRepository.getAnimeById(id)
-        if (anime != null) {
-            loadAnimeDetails(anime)
-        } else {
-            _anime.value = null
-        }
+    fun loadAnimeById(id: Int) {
+        _anime.value = MockAnimeRepository.getAnimeById(id)
     }
 }
