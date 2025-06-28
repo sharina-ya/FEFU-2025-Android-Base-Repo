2a2657a51b1e7d5d15e5ad78afc2f8edce3ed3b6
sharina-ya <uliasarina7@gmail.com>
2025-06-28 19:42:31 +1000

    Add files via upload

--- app/src/main/java/co/feip/fefu2025/domian/usecase/GetAnimeDetailsUseCase.kt
+++ app/src/main/java/co/feip/fefu2025/domian/usecase/GetAnimeDetailsUseCase.kt
@@ -1,9 +1,10 @@
 package co.feip.fefu2025.domain.usecase
 
 import co.feip.fefu2025.domain.model.Anime
+import co.feip.fefu2025.domain.repository.AnimeRepository
 
-class GetAnimeDetailsUseCase {
-    fun execute(anime: Anime): Anime {
-        return anime
+class GetAnimeDetailsUseCase(private val repository: AnimeRepository) {
+    fun execute(anime: Anime): Anime? {
+        return repository.getAnimeById(anime.id)
     }
 }
