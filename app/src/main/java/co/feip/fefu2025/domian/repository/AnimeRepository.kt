b726b52f1849b927deb35c68b98d066abee54c11
sharina-ya <uliasarina7@gmail.com>
2025-06-29 14:35:50 +1000

    Add files via upload

--- app/src/main/java/co/feip/fefu2025/domian/repository/AnimeRepository.kt
+++ app/src/main/java/co/feip/fefu2025/domian/repository/AnimeRepository.kt
@@ -3,6 +3,6 @@ package co.feip.fefu2025.domain.repository
 import co.feip.fefu2025.domain.model.Anime
 
 interface AnimeRepository {
-    fun getAnimeList(): List<Anime>
-    fun getAnimeById(id: Int): Anime?
+    suspend fun getAnimeList(): List<Anime>
+    suspend fun getAnimeById(id: Int): Anime?
 }
