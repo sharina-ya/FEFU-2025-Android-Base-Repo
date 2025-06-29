6a8178f765fbb390e72091645755b2ec1b52f3ca
sharina-ya <uliasarina7@gmail.com>
2025-06-29 14:36:28 +1000

    Add files via upload

--- app/src/main/java/co/feip/fefu2025/domian/usecase/GetAnimeListUseCase.kt
+++ app/src/main/java/co/feip/fefu2025/domian/usecase/GetAnimeListUseCase.kt
@@ -4,5 +4,5 @@ import co.feip.fefu2025.domain.model.Anime
 import co.feip.fefu2025.domain.repository.AnimeRepository
 
 class GetAnimeListUseCase(private val repository: AnimeRepository) {
-    fun execute(): List<Anime> = repository.getAnimeList()
+    suspend fun execute(): List<Anime> = repository.getAnimeList()
 }
