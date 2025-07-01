5e4f83f635316ee5b1eef490b70724f534fc7875
sharina-ya <uliasarina7@gmail.com>
2025-07-02 06:00:55 +1000

    Add files via upload

--- app/src/main/java/co/feip/fefu2025/presentation/navigation/AppNavGraph.kt
+++ app/src/main/java/co/feip/fefu2025/presentation/navigation/AppNavGraph.kt
@@ -6,15 +6,9 @@ import androidx.navigation.compose.*
 import androidx.navigation.navArgument
 import androidx.navigation.navDeepLink
 import co.feip.fefu2025.presentation.animeDetails.AnimeDetailsScreen
+import co.feip.fefu2025.presentation.animeDetails.RecommendationsScreen
 import co.feip.fefu2025.presentation.animeList.AnimeListScreen
 import co.feip.fefu2025.presentation.animeList.SearchScreen
-
-object Destinations {
-    const val MAIN = "main"
-    const val DETAILS = "details/{animeId}"
-    const val SEARCH = "search"
-    fun details(animeId: Int) = "details/$animeId"
-}
 
 @Composable
 fun AppNavGraph() {
@@ -30,20 +24,6 @@ fun AppNavGraph() {
                 }
             )
         }
-        composable(
-            "animeDetails/{animeId}",
-            arguments = listOf(navArgument("animeId") { type = NavType.IntType })
-        ) { backStackEntry ->
-            val animeId = backStackEntry.arguments?.getInt("animeId") ?: 0
-            AnimeDetailsScreen(
-                animeId = animeId,
-                onAnimeClick = { newId ->
-                    // Навигация на новый экран деталей с другим animeId
-                    navController.navigate("animeDetails/$newId")
-                },
-                onBack = { navController.popBackStack() }
-            )
-        }
 
         composable("search") {
             SearchScreen(
@@ -51,6 +31,28 @@ fun AppNavGraph() {
                 onAnimeClick = { animeId ->
                     navController.navigate("animeDetails/$animeId")
                 }
+            )
+        }
+
+        composable(
+            "animeDetails/{animeId}",
+            arguments = listOf(navArgument("animeId") { type = NavType.IntType })
+        ) { backStackEntry ->
+            val animeId = backStackEntry.arguments?.getInt("animeId") ?: 0
+            AnimeDetailsScreen(
+                animeId = animeId,
+                onAnimeClick = { newId -> navController.navigate("animeDetails/$newId") },
+                onBack = { navController.popBackStack() },
+                onRecommendationsClick = {
+                    navController.navigate("recommendations")
+                }
+            )
+        }
+
+        composable("recommendations") {
+            RecommendationsScreen(
+                onAnimeClick = { id -> navController.navigate("animeDetails/$id") },
+                onBack = { navController.popBackStack() }
             )
         }
     }
